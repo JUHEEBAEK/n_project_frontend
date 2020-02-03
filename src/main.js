1b4cea7dbc4068cec01be080fb430b8ad5b459b1
Juhee Baek <juhee.baek@fitogether.com>
2020-02-03 21:13:15 +0900

    error 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { store } from "./store";
+import {
+  store
+} from "./store";
 import "./registerServiceWorker";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@fortawesome/fontawesome-free/css/all.css";
@@ -12,6 +14,17 @@ import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.prototype.moment = moment;
+
+const ignoreWarnMessage =
+  "The .native modifier for v-on is only valid on components but it was used on <div>.";
+Vue.config.warnHandler = function (msg, vm, trace) {
+  // `trace` is the component hierarchy trace
+  if (msg === ignoreWarnMessage) {
+    msg = null;
+    vm = null;
+    trace = null;
+  }
+}
 
 new Vue({
   router,
