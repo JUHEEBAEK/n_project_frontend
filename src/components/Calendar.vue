ec7c8c35f12ddb70c6db7b766d91ec5f306199b5
Juhee Baek <juhee.baek@fitogether.com>
2020-02-03 21:13:36 +0900

    calendar event popup ui

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,110 +1,184 @@
 <template>
-  <div>
-    <v-row class="fill-height">
-      <v-col>
-        <v-sheet height="64">
-          <v-toolbar flat color="white">
-            <v-btn
-              outlined
-              class="mr-4"
-              color="grey darken-2"
-              @click="setToday"
-            >
-              Today
-            </v-btn>
-            <v-btn fab text small color="grey darken-2" @click="prev">
-              <v-icon small>fas fa-chevron-left</v-icon>
-            </v-btn>
-            <v-btn fab text small color="grey darken-2" @click="next">
-              <v-icon small>as fa-chevron-right</v-icon>
-            </v-btn>
-            <v-toolbar-title>{{ title }}</v-toolbar-title>
-            <v-spacer />
-            <v-menu bottom right>
-              <template v-slot:activator="{ on }">
-                <v-btn outlined color="grey darken-2" v-on="on">
-                  <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right>fas fa-caret-down</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item @click="type = 'month'">
-                  <v-list-item-title>Month</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-toolbar>
-        </v-sheet>
-        <v-sheet height="600">
-          <v-calendar
-            ref="calendar"
-            v-model="focus"
-            color="primary"
-            :events="events"
-            :event-color="getEventColor"
-            :now="today"
-            :type="type"
-            @click:event="showEvent"
-            @click:more="viewDay"
-            @click:date="viewDay"
-            @change="updateRange"
-          />
-          <v-menu
-            v-model="selectedOpen"
-            :close-on-content-click="false"
-            :activator="selectedElement"
-            offset-x
-          >
-            <v-card color="grey lighten-4" min-width="350px" flat>
-              <v-toolbar :color="selectedEvent.color" dark>
-                <v-btn icon>
-                  <v-icon>fas fa-pencil-alt</v-icon>
-                </v-btn>
-                <v-toolbar-title v-html="selectedEvent.name" />
-                <v-spacer />
-                <v-btn icon>
-                  <v-icon>fas fa-heart</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>fas fa-ellipsis-v</v-icon>
-                </v-btn>
-              </v-toolbar>
-              <v-card-text>
-                <span v-html="selectedEvent.details" />
-              </v-card-text>
-              <v-card-actions>
-                <v-btn text color="secondary" @click="selectedOpen = false">
-                  Cancel
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-menu>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </div>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar flat dense color="white">
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            Today
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small>fas fa-chevron-left</v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small>fas fa-chevron-right</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          type="month"
+          :events="events"
+          :event-color="getEventColor"
+          :now="today"
+          @click:event="showEvent"
+          @change="updateRange"
+        />
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card color="grey lighten-4" min-width="350px" flat>
+            <v-toolbar color="grey lighten-4" flat tile height="40px">
+              <v-spacer />
+              <v-btn icon small class="mx-1">
+                <v-icon small class="toolbar__icon">fas fa-edit</v-icon>
+              </v-btn>
+              <v-btn icon small class="mx-1">
+                <v-icon small class="toolbar__icon">fas fa-trash-alt</v-icon>
+              </v-btn>
+              <v-btn icon small class="mx-1"  @click="selectedOpen = false">
+                <v-icon small>fas fa-times</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text class="pa-2">
+              <div class="item__container">
+                <div class="row">
+                  <div class="col-2 align-self-center">
+                    <v-avatar size="20" :color="selectedEvent.color" />
+                  </div>
+                  <div class="col-10 text-left">
+                    <div class="row">
+                      <div class="col-12 py-0">
+                      <span class="schedule__title" v-html="selectedEvent.name" />
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-12 py-0">
+                        <span class="schedule__start"
+                              v-html="selectedEvent.start" /> ~
+                        <span class="schedule__end"
+                              v-html="selectedEvent.end" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item__container">
+                <div class="row">
+                  <div class="col-2 align-self-center">
+                    <v-icon>fas fa-map-marker-alt</v-icon>
+                  </div>
+                  <div class="col-10 text-left">
+                    <div class="row">
+                      <div class="col-12 py-0">
+                        <span class="schedule__place"
+                              v-html="selectedEvent.place" />
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-12 py-0">
+                        <span class="schedule__address"
+                             v-html="selectedEvent.address" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item__container">
+                <div class="row">
+                  <div class="col-2">
+                    <v-icon>fas fa-users</v-icon>
+                  </div>
+                  <div class="col-10 text-left">
+                    <div class="row">
+                      <div class="col-12 py-0">
+                         <span class="schedule__attendance"
+                               v-html="`참석자 ${selectedEvent.attendCount} 명`" />
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-12 py-0">
+                        <span class="attendance__member"
+                              v-for="(item, idx) in selectedEvent.attend"
+                              :key="idx">
+                          {{ item }}
+                        </span>
+
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   data: () => ({
     focus: "",
-    type: "month",
     today: new Date().toISOString().substr(0, 10),
-    typeToLabel: {
-      month: "Month"
-    },
     start: null,
     end: null,
+    selectedOpen: false,
     selectedEvent: {},
     selectedElement: null,
-    selectedOpen: false,
-    events2: [],
     events: [
-      { name: "훈련 + 자체 경기 우면 다목적 구장", start: "2020-01-04 15:00", end: "2020-01-04 17:00", color: "green", type: "training"},
-      { name: "자체 경기 잠실 제 2구장", start: "2020-01-11 17:00", end: "2020-01-11 19:00", color: "indigo", type: "practice"},
-      { name: "친선 경기 잠실 제 3구장", start: "2020-01-18 17:00", end: "2020-01-18 19:00", color: "orange", type: "game"},
-      { name: "훈련 + 자체 경기 풀굿", start: "2020-01-25 15:00", end: "2020-01-25 18:00", color: "green", type: "training"},
+      {
+        name: "[훈련] 우면 다목적 구장",
+        start: "2020-01-04 15:00",
+        end: "2020-01-04 17:00",
+        place: "우면 다목정 구장",
+        address: "서울 서초구 우면동 440-5",
+        attendCount: 2,
+        attend: ["백주희, 이화인"],
+        color: "green",
+        type: "training"
+      },
+      {
+        name: "[경기] 잠실 제 2구장",
+        start: "2020-01-11 17:00",
+        end: "2020-01-11 19:00",
+        place: "잠실 제 2구장",
+        address: "서울특별시 송파구 올림픽로 25",
+        attendCount: 10,
+        attend: ["백주희", "이현아", "조명선, 이화인", "이종은", "김나경", "이지윤", "박채민", "원지향", "김지현"],
+        color: "indigo",
+        type: "practice"
+      },
+      {
+        name: "[친선] 잠실 제 3구장",
+        start: "2020-01-18 17:00",
+        end: "2020-01-18 19:00",
+        place: "잠실 제 3구장",
+        address: "서울특별시 송파구 올림픽로 25",
+        attendCount: 8,
+        attend: ["백주희", "이현아", "조명선, 이화인", "이종은", "김나경", "이지윤", "박채민",],
+        color: "orange",
+        type: "game"
+      },
+      {
+        name: "[훈련] 풀굿",
+        start: "2020-01-25 15:00",
+        end: "2020-01-25 18:00",
+        place: "풀굿",
+        address: "서울시 관악구 은천동 봉천두산아파트 208동 상가 지하",
+        attendCount: 6,
+        attend: ["백주희", "이현아", "최주희", "이화인", "이종은", "김나경"],
+        color: "green",
+        type: "training"
+      }
     ],
     colors: [
       "blue",
@@ -128,6 +202,7 @@ export default {
   }),
   computed: {
     title() {
+      console.log("title");
       const { start, end } = this;
       if (!start || !end) {
         return "";
@@ -135,7 +210,6 @@ export default {
 
       const startMonth = this.monthFormatter(start);
       const startYear = start.year;
-
       return `${startMonth} ${startYear}`;
     },
     monthFormatter() {
@@ -149,11 +223,6 @@ export default {
     this.$refs.calendar.checkChange();
   },
   methods: {
-    viewDay({ date }) {
-      console.log('select viewDay');
-      this.focus = date;
-      console.log(this.focus);
-    },
     getEventColor(event) {
       return event.color;
     },
@@ -166,66 +235,45 @@ export default {
     next() {
       this.$refs.calendar.next();
     },
-    showEvent({ nativeEvent, event }) {
+    showEvent ({ nativeEvent, event }) {
       const open = () => {
-        this.selectedEvent = event;
-        this.selectedElement = nativeEvent.target;
-        setTimeout(() => (this.selectedOpen = true), 10);
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        setTimeout(() => this.selectedOpen = true, 10)
       };
 
       if (this.selectedOpen) {
-        this.selectedOpen = false;
-        setTimeout(open, 10);
+        this.selectedOpen = false
+        setTimeout(open, 10)
       } else {
-        open();
+        open()
       }
 
-      nativeEvent.stopPropagation();
+      nativeEvent.stopPropagation()
     },
     updateRange({ start, end }) {
-      const events = [];
-/*
-
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: this.formatDate(first, !allDay),
-          end: this.formatDate(second, !allDay),
-          color: this.colors[this.rnd(0, this.colors.length - 1)]
-        });
-      }
-*/
-
+      console.log("updateRange");
       this.start = start;
       this.end = end;
     },
-    nth(d) {
-      return d > 3 && d < 21
-        ? "th"
-        : ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"][d % 10];
-    },
-    /*rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
-    formatDate(a, withTime) {
-      return withTime
-        ? `${a.getFullYear()}-${a.getMonth() +
-            1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
-        : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`;
-    }*/
+    close() {
+      console.log("닫아줘");
+      this.selectedOpen = false
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style>
+.toolbar__icon {
+  font-weight: 400;
+}
+.schedule__place {
+  color: #000;
+}
+.schedule__title{
+  font-weight: 600;
+  font-size: 18px;
+}
+
+</style>
