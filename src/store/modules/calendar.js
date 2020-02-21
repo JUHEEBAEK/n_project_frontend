5e80570df360d265a9fefa5176bc4c1994042445
Cheol_Kim <cheol.kim@fitogether.com>
2020-02-21 22:05:46 +0900

    Make Vuex Function to load attendance

--- src/store/modules/calendar.js
+++ src/store/modules/calendar.js
@@ -5,6 +5,9 @@ import {
   addEvent,
   selectEvent
 } from "../../api/schedule.js";
+import {
+  attendanceList
+} from "../../api/attend.js";
 
 import moment from "moment";
 import * as constants from "../constants";
@@ -20,56 +23,69 @@ const mutations = {
   [constants.setNewEventModal]: set("newEventBox"),
   [constants.setFullEventModal]: set("fullEventDialog"),
   SET_EVENT_LIST(state, payload) {
-    let event_dict = {}; // dict로 넣어주는게 편하다
     let event_list = [];
-    let id_list = [];
     for (var index in payload) {
       let item = payload[index];
-      if (item["id"] in event_dict) {
-        // id가 있는 경우 member만 push 해준다
-        let existing_item = event_dict[item["id"]];
-        existing_item["member_id_list"].push(item["member_id"]);
-        existing_item["memeber_name_list"].push(item["member_name"]);
-        existing_item["attendCount"]++;
-        event_dict[item["id"]] = existing_item;
-      } else {
-        // id가 없는 경우 전부 추가 (member id와 name는 list형태로 집어넣는다)
-        item["member_id_list"] = [item["member_id"]];
-        item["memeber_name_list"] = [item["member_name"]];
-        // name이 없는 경우, 임의로 만들어서 넣어준다
-        if (item["name"] == null) {
-          // item["name"] = [경기] 잠실 n 구장
-          item["name"] = `[${item["type"]}] ${item["stadium_name"]}`;
-        }
-        //attendCount 필요함
-        item["attendCount"] = 1;
-        // place에 stadium_name
-        item["place"] = item["stadium_name"];
-        // start랑 end가 없고 start_time end_time이 있음
-        let date = moment(item["date"]);
-        if (item["start_time"] == null) {
-          item["start"] = date.format("YYYY-MM-DD HH:mm");
-          item["end"] = date.format("YYYY-MM-DD HH:mm");
-        } else {
-          item["start"] = item["start_time"];
-          item["end"] = item["end_time"];
-        }
-        item["date"] = moment(item["date"]).format("YYYY-MM-DD");
 
-        item["color"] = "grape"; // 적당한 색으로 넣어준다
-        //open은 default false로 설정해준다
-        item["open"] = false;
-        //넣어주기
-        event_dict[item["id"]] = item;
+      // id가 없는 경우 전부 추가 (member id와 name는 list형태로 집어넣는다)
+      item["member_id_list"] = [];
+      item["memeber_name_list"] = [];
+
+      // name이 없는 경우, 임의로 만들어서 넣어준다
+      if (item["name"] == null) {
+        // ex) item["name"] = [경기] 잠실 n 구장
+        item["name"] = `[${item["type"]}] ${item["stadium_name"]}`;
+      }
+      //attendCount 필요함
+      item["attendCount"] = 0;
+      let date = moment(item["date"]);
+      item["date"] = moment(item["date"]).format("YYYY-MM-DD");
+
+      item["color"] = "grape"; // 적당한 색으로 넣어준다
+      //open은 default false로 설정해준다
+      item["open"] = false;
+      //넣어주기
+      event_list.push(item);
+    }
+    state.eventList = event_list;
+  },
+  ADD_MEMBER_LIST(state, memberList) {
+    console.log("ADD_MEMBER_LIST", memberList);
+    if (memberList.length == 0) {
+      return false;
+    }
+    let memeber_name_list = [];
+    let memeber_id_list = [];
+    let member_count = memberList.length;
+    for (let i in memberList) {
+      memeber_id_list.push(memberList[i]["member_id"]);
+      memeber_name_list.push(memberList[i]["name"]);
+    }
+
+    let schedule_id = memberList[0]["schedule_id"];
+    // eventList에서 id가 일치하는 list를 찾은 다음에 가져오기
+    let index = -1;
+    for (let i in state.eventList) {
+      let id_check = state.eventList[i]["id"];
+      if (id_check == schedule_id) {
+        index = i;
+        break;
       }
     }
-    console.log("mutation SET_EVENT_LIST");
-    for (var key in event_dict) {
-      event_list.push(event_dict[key]);
+    if (index == -1) {
+      console.log("ADD_MEMBER_LIST, can not find Schedule")
+      return
     }
-    console.log(event_list);
-    state.eventList = event_list;
-  }
+    console.log(state.eventList)
+    console.log(index, state.eventList[index])
+    let item = state.eventList[index];
+    item["attendCount"] = member_count;
+    item["memeber_id_list"] = memeber_id_list;
+    item["memeber_name_list"] = memeber_name_list;
+
+    state.eventList[index] = item;
+    console.log(state.eventList[index])
+  },
 };
 
 const actions = {
@@ -93,6 +109,10 @@ const actions = {
     } catch (e) {
       console.log(e);
     }
+  },
+  async load_member(context, id) {
+    const attend_list = await attendanceList(id);
+    context.commit("ADD_MEMBER_LIST", attend_list["data"]);
   }
 };
 
