52547bbbbc4da020371728e89c1c63437c24e732
Cheol_Kim <cheol.kim@fitogether.com>
2020-03-22 19:39:10 +0900

    Make Squad Store Module

---
+++ src/store/modules/squad.js
@@ -0,0 +1,82 @@
+const state = {
+    team_division: {
+        teams: [],
+        jocker_player: null
+    }
+};
+
+const mutations = {
+    divide_member_into_team(state, {
+        attend_member_id_list,
+        number_team,
+        min_player,
+        jocker_exist
+    }) {
+        // initialize
+        state.team_division = {
+            teams: [],
+            jocker_player: null
+        }
+        if (isInputErrorExist(attend_member_id_list,
+                number_team,
+                min_player,
+                jocker_exist)) {
+            return false;
+        }
+
+        // jocker가 있는 경우 한명을 뺌
+        if (jocker_exist) {
+            state.team_division.jocker_player = array_random_extract(attend_member_id_list, 1)[0]
+        }
+
+        while (attend_member_id_list && number_team) {
+            // 뺄 사람 수를 정한다
+            let number_of_player_in_team = Math.ceil(attend_member_id_list.length / number_team--);
+            let random_member_id_list = array_random_extract(attend_member_id_list, number_of_player_in_team);
+            state.team_division.teams.push(random_member_id_list);
+        }
+        return true
+        // 위에 필요한 함수
+        function isInputErrorExist(attend_member_id_list,
+            number_team,
+            min_player,
+            jocker_exist) {
+
+            let needed_number_of_player = 0
+            if (jocker_exist) {
+                needed_number_of_player = number_team * min_player + 1
+            } else {
+                needed_number_of_player = number_team * min_player
+            }
+            return attend_member_id_list.length < needed_number_of_player
+        }
+
+        function array_random_extract(original_array, number_of_extraction) {
+            let extracted_array = []
+            for (let i = 0; i < number_of_extraction; i++) {
+
+                let random_selected_index = getRandomInt(0, original_array.length - 1)
+
+                extracted_array.push(original_array[random_selected_index])
+                original_array.splice(random_selected_index, 1)
+            }
+            return extracted_array
+        }
+
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+        }
+    },
+};
+
+const actions = {
+
+};
+
+export default {
+    state,
+    mutations,
+    actions
+};
