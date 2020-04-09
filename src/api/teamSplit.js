a48cc5b6368f23d0d2ad488711e4970485af6196
Cheol_Kim <cheol.kim@fitogether.com>
2020-04-09 11:35:46 +0900

    FIx Indent

--- src/api/teamSplit.js
+++ src/api/teamSplit.js
@@ -13,58 +13,67 @@ async function createTeamSplit(form) {
 }
 
 async function getTeamSplitList() {
-    try {
-      const response = await axios.get(`${devUrl}/api/teamSplit/list`);
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
+  try {
+    const response = await axios.get(`${devUrl}/api/teamSplit/list`);
+    return response;
+  } catch (e) {
+    console.log(e);
   }
-  
-  async function getInfo(schduleId) {
-    try {
-      const response = await axios.get(`${devUrl}/api/teamSplit/getInfo/${schduleId}`);
-  
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
+}
+async function getSplitTeamListWithSchedule(scheduleId) {
+  try {
+    const response = await axios.get(`${devUrl}/api/teamSplit/getSplitTeamListWithSchedule/${scheduleId}`);
+    return response;
+  } catch (e) {
+    console.log(e);
   }
+}
   
-  async function deleteTeamSplit(teamSplit_id_form) {
-    try {
-      const response = await axios.delete(`${devUrl}/api/teamSplit/delete`, {
-        data: teamSplit_id_form
-      });
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  
-  async function updateTeamSplit(formData) {
-    try {
-      const response = await axios.put(`${devUrl}/api/teamSplit/update`, formData)
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+async function getInfo(teamSplitId) {
+  try {
+    const response = await axios.get(`${devUrl}/api/teamSplit/getInfo/${teamSplitId}`);
 
-  async function bulkCreateOrUpdate(formData){
-    try {
-      const response = await axios.post(`${devUrl}/api/teamSplit/bulkCreateOrUpdate`, formData)
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
+    return response;
+  } catch (e) {
+    console.log(e);
   }
-  
-  export {
-    createTeamSplit,
-    getTeamSplitList,
-    getInfo,
-    deleteTeamSplit,
-    updateTeamSplit,
-    bulkCreateOrUpdate
-  };
+}
+
+async function deleteTeamSplit(teamSplit_id_form) {
+  try {
+    const response = await axios.delete(`${devUrl}/api/teamSplit/delete`, {
+      data: teamSplit_id_form
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function updateTeamSplit(formData) {
+  try {
+    const response = await axios.put(`${devUrl}/api/teamSplit/update`, formData)
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function bulkCreateOrUpdate(formData){
+  try {
+    const response = await axios.post(`${devUrl}/api/teamSplit/bulkCreateOrUpdate`, formData)
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export {
+  createTeamSplit,
+  getTeamSplitList,
+  getSplitTeamListWithSchedule,
+  getInfo,
+  deleteTeamSplit,
+  updateTeamSplit,
+  bulkCreateOrUpdate
+};
