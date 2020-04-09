5ae9c2330d39942bbb7d586faee6f0bb2db7dee9
Juhee Baek <juhee.baek@fitogether.com>
2020-04-09 09:38:09 +0900

    toolbar , nav attach

--- src/views/nav/squad/MatchPrepare.vue
+++ src/views/nav/squad/MatchPrepare.vue
@@ -1,21 +1,24 @@
 <template>
-  <v-row class="match__container" justify="center">
-    <v-col cols="12" lg="8" md="9" sm="12" xs="12">
-      <v-contatner fluid>
-        <v-form ref="form">
-          <squad-date-and-quarter></squad-date-and-quarter>
-          <squad-team-list></squad-team-list>
-          <squad-input-position></squad-input-position>
-
-          <v-row>
-            <v-col>
-              <v-btn @click="save" color="primary">저장</v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
-      </v-contatner>
-    </v-col>
-  </v-row>
+  <div>
+    <core-Toolbar />
+    <core-Navigation />
+    <v-row class="match__container" justify="center">
+      <v-col cols="12" lg="8" md="9" sm="12" xs="12">
+        <v-contatner fluid>
+          <v-form ref="form">
+            <squad-date-and-quarter></squad-date-and-quarter>
+            <squad-team-list></squad-team-list>
+            <squad-input-position></squad-input-position>
+            <v-row>
+              <v-col>
+                <v-btn @click="save" color="primary">저장</v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-contatner>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -25,10 +28,9 @@ export default {
       console.log("SAVE");
       if (this.$refs.form.validate()) {
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
