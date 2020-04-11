cf3cdc5b2a0bd310e060b65c02bdbcde594bdb0b
Cheol_Kim <cheol.kim@fitogether.com>
2020-04-12 00:29:14 +0900

    Add get(), set() on State Variable "teamSplitSelcted - v-model with Store State reference: https://vuex.vuejs.org/kr/guide/forms.html

--- src/components/squad/TeamSplitList.vue
+++ src/components/squad/TeamSplitList.vue
@@ -2,7 +2,7 @@
   <v-card class="py-2">
     <v-card-title>팀 나누기 목록</v-card-title>
     <v-card-text>
-      <v-select v-model="temaSplitSelcted" :items="teamSplitList" @change="SET_TEAM_INDEX_CHANGED" />
+      <v-select v-model="temaSplitSelcted" :items="teamSplitList" />
     </v-card-text>
   </v-card>
 </template>
@@ -22,7 +22,15 @@ export default {
   data: () => ({
   }),
   computed: {
-    ...squadState(["teamSplitList", "temaSplitSelcted"]),
+    ...squadState(["teamSplitList"]),
+    temaSplitSelcted:{
+      get(){
+        return  this.$store.state.squad.temaSplitSelcted
+      },
+      set(value){
+        this.SET_TEAM_INDEX_CHANGED(value)
+      }
+    }
   },
   
   methods: {
