fc2a7a61155eb8c3ed3444674f698c64e5ff365a
Cheol_Kim <cheol.kim@fitogether.com>
2020-04-20 01:09:14 +0900

    Add API squad.js

---
+++ src/api/squad.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+// const baseUrl = 'http://15.164.138.118:3000';
+const devUrl = "http://localhost:3000";
+
+
+async function getinfoSquad(id) {
+    try {
+        const response = await axios.get(`${devUrl}/api/squad/getinfo/${id}`);
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function createSquad(form) {
+    try {
+        const response = await axios.post(
+            `${devUrl}/api/squad/create`,
+            form
+        );
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function deleteSquad(form) {
+    try {
+        const response = await axios.delete(
+            `${devUrl}/api/squad/delete`, {
+                data: form
+            }
+        );
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function getSquadList() {
+    try {
+        const response = await axios.get(`${devUrl}/api/squad/list`);
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export {
+    getinfoSquad,
+    createSquad,
+    deleteSquad,
+    getSquadList,
+};
