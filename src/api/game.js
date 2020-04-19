7bc05a73351748aa9ff2316f061190d4be34358a
Cheol_Kim <cheol.kim@fitogether.com>
2020-04-20 00:25:18 +0900

    Add game API

---
+++ src/api/game.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+
+// const baseUrl = 'http://15.164.138.118:3000';
+const devUrl = "http://localhost:3000";
+
+async function getinfoGame(id) {
+    try {
+        const response = await axios.get(`${devUrl}/api/game/getinfo/${id}`);
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function searchWithScheduleIdAndQuarter(form){
+    try {
+        const response = await axios.post(
+            `${devUrl}/api/game/searchWithScheduleIdAndQuarter`,
+            form
+        );
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function createGame(form) {
+    try {
+        const response = await axios.post(
+            `${devUrl}/api/game/create`,
+            form
+        );
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function deleteGame(form) {
+    try {
+        const response = await axios.delete(
+            `${devUrl}/api/game/delete`, {
+                data: form
+            }
+        );
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function getGameList() {
+    try {
+        const response = await axios.get(`${devUrl}/api/game/list`);
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export {
+    getinfoGame,
+    createGame,
+    deleteGame,
+    getGameList,
+    searchWithScheduleIdAndQuarter,
+};
