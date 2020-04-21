1d5a0e3c41075ea8ba001fd98ac06965f68d84e0
Cheol_Kim <cheol.kim@fitogether.com>
2020-04-21 21:32:57 +0900

    Change QuarterMax 6 -> 10

--- src/store/modules/prepareMatch.js
+++ src/store/modules/prepareMatch.js
@@ -5,7 +5,7 @@ import moment from "moment";
 
 const state = {
   date: moment().format("YYYY-MM-DD"),
-  quarterList: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6"],
+  quarterList: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6","Q7","Q8","Q9","Q10"],
   quarterIndex: 0,
   isHome: true,
   selectType: "Home",
