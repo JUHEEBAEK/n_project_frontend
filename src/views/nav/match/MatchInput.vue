b53958d6e1a236739f2af203020d6b271e3edc2f
Juhee Baek <juhee.baek@fitogether.com>
2020-06-08 11:00:25 +0900

    코드 안쓰는 것 빼기

--- src/views/nav/match/MatchInput.vue
+++ src/views/nav/match/MatchInput.vue
@@ -14,7 +14,7 @@
       <!--필드 그림:: 선수 선택 페이지 -->
       <v-row class="event__main">
         <!-- 경기 기록 페이지 -->
-        <match-event-input></match-event-input>
+        <match-event-input :scheduleInfo="scheduleInfo"></match-event-input>
         <match-event-list></match-event-list>
       </v-row>
     </v-container>
@@ -28,33 +28,22 @@ import Position from "../../../assets/value/Postion.json";
 
 import { createNamespacedHelpers } from "vuex";
 const {
-  mapState: calendarMapState,
-  mapGetters: calendarMapGetters,
-  mapMutations: calendarMapMutations,
-  mapActions: calendarMapActions,
-} = createNamespacedHelpers("calendar");
-const {
   mapState: squadState,
   mapMutations: squadMutations,
-  mapActions: squadActions,
+  mapActions: squadActions
 } = createNamespacedHelpers("squad");
-const {
-  mapState: prepareMatchState,
-  mapGetters: prepareMatchGetters,
-  mapMutations: prepareMatchMutations,
-  mapActions: prepareMatchActions,
-} = createNamespacedHelpers("prepareMatch");
 
 export default {
   filters: {
     setMomentMonth: function(val) {
       return moment(val).format("MMM");
-    },
+    }
   },
   data: () => ({
     // 스케쥴 리스트 영역
     scheduleList: [],
     scheduleIndex: null,
+    scheduleInfo: {},
     setYear: moment().format("YYYY"),
     setMonth: moment().format("MMMM"),
     setDay: moment().format("DD"),
@@ -74,10 +63,9 @@ export default {
     positionAwayList: Position.awayList,
     position: Position.basicPostion,
     homePlayerList: [],
-    awayPlayerList: [],
+    awayPlayerList: []
   }),
   computed: {
-    ...prepareMatchState(["homeMembers", "awayMembers"]),
     setStatus() {
       this.init();
       if (this.isGoal) {
@@ -89,47 +77,33 @@ export default {
         this.lastEventType = "In";
         return "Change Player";
       }
-    },
+    }
   },
   watch: {
     scheduleIndex: async function(val) {
+      console.log(val);
       if (val) {
         let selected_schedule = this.scheduleList[this.scheduleIndex];
         this.setDateString(selected_schedule);
-        // this.$emit("changeDate", selected_schedule);
+        this.scheduleInfo = selected_schedule;
       }
     },
     quarterCount: function() {
       this.quarterIndex =
         this.scheduleList[this.scheduleIndex]["quarterCount"] - 1;
-    },
-  },
-  async created() {
-    await this.setScheduleList();
+    }
   },
   methods: {
     ...squadActions(["getSplitTeamListWithSchedule"]),
-    ...prepareMatchActions(["getHomeAwayMember"]),
     setDateString(selected_schedule) {
       this.setYear = moment(selected_schedule.date).format("YYYY");
       this.setMonth = moment(selected_schedule.date).format("MMMM");
       this.setDay = moment(selected_schedule.date).format("DD");
     },
-    setInfo(item) {
-      // TODO: 스케쥴 정보 선택 API 호출
-      this.quarterCount = item.quarterCount;
-    },
-    setScheduleList() {
-      this.scheduleList = dummyData.MatchInput_scheduleList;
-      // 스케쥴 초기 세팅
-      this.scheduleIndex = this.scheduleList.length - 1;
-      // 쿼터 초기 세팅
-      this.quarterCount = this.scheduleList[this.scheduleIndex]["quarterCount"];
-    },
     async setScheduleData(selected_schedule) {
       if (this.scheduleIndex == -1) return;
-    },
-  },
+    }
+  }
 };
 </script>
 
