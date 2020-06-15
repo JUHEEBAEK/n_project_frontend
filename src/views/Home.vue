93584c740c919a1a11c18624232dffc35c080332
Juhee Baek <juhee.baek@fitogether.com>
2020-06-15 15:28:41 +0900

    test add h2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <h2>hello tester {{ message }}</h2>
+    <h2>Good!!!!</h2>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    message: "Wolrd!! second test!",
-  }),
+    message: "Wolrd!! second test!"
+  })
 };
 </script>
 
