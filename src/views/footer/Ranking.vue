5a2f902ab27108817468adeec0c851c3f82ced57
Cheol_Kim <cheol.kim@fitogether.com>
2020-06-28 18:04:01 +0900

    Update UI

--- src/views/footer/Ranking.vue
+++ src/views/footer/Ranking.vue
@@ -1,9 +1,62 @@
 <template>
   <div>
-    <div class="filter-button">
-      <v-btn> 대회 </v-btn>
-      <v-btn> 친선 </v-btn>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="selectHeader">
+            <b>경기 종류</b>
+          </div>
+        </v-col>
+        <v-col>
+        <div class="selectHeader">
+            <b>연도별</b>
+          </div>
+        </v-col>
+        <v-col>
+        <div class="selectHeader">
+            <b>월별</b>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div class="selectBody">
+            <v-radio-group v-model="selectedContest">
+              <v-radio
+                v-for="(contest, index) in contests"
+                :key="`contest-${index}`"
+                :label="contest"
+                :value="contest"
+              ></v-radio>
+            </v-radio-group>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="selectBody">
+            <v-radio-group v-model="selectedYear">
+              <v-radio
+                v-for="(year, index) in years"
+                :key="`year-${index}`"
+                :label="year"
+                :value="year"
+              ></v-radio>
+            </v-radio-group>
+          </div>
+        </v-col>
+        <v-col> 
+          <div class="selectBody">
+            <v-radio-group v-model="selectedMonth">
+              <v-radio
+                v-for="(month, index) in months"
+                :key="`month-${index}`"
+                :label="month"
+                :value="month"
+              ></v-radio>
+            </v-radio-group>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
     <div class="filter-date">
       <v-select :items="years" label="Year" solo></v-select>
       <v-select :items="months" label="Month" solo></v-select>
@@ -40,8 +93,14 @@ export default {
     return {
       tab: null,
       tabItems: ["GOAL", "ASSIST", "CLEAN_SHEET"],
+
+      contests: ["대회", "친선"],
       years: ["전체", 2018, 2019, 2020],
-      monrths: ["전체", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+      months: ["전체", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+
+      selectedContest: "대회",
+      selectedYear: "전체",
+      selectedMonth: "전체"
     };
   },
   computed: {
@@ -62,4 +121,22 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.filter-button {
+}
+.selectBody{
+  overflow-y: auto;
+  height: 200px;
+  border-right: 1px solid #d5d8d8;
+  border-left: 1px solid #d5d8d8;
+  padding: 2px 8px;
+}
+.selectHeader{
+  padding: 8px 0;
+  border-bottom: 1px solid #d5d8d8;
+  text-align: left;
+  font-weight: normal;
+  text-indent: 20px;
+  font-size: 20px;
+}
+</style>
