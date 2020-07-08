c44376bc90c67d5ef272c3519190e1261cb5fb64
Juhee Baek <juhee.baek@fitogether.com>
2020-07-08 11:36:07 +0900

    gameEventList 모바일 화면 잘 보이도록

--- src/components/report/EventList.vue
+++ src/components/report/EventList.vue
@@ -6,33 +6,44 @@
         <span class="report__score">{{ `${homeScore} : ${awayScore} `}}</span>
       </v-card-title>
       <v-card-text class="report__content">
-        <v-timeline>
-          <v-timeline-item
-            v-for="(event, i) in gameEventList"
-            :key="i"
-            color="#187c5a"
-            fill-dot
-            small
-            :right="event.team_type === 'A' ? true : false"
-            :left="event.team_type === 'H' ? true : false"
-          >
-            <template v-slot:opposite>
-              <span class="event__time-text" v-text="event.event_time"></span>
-            </template>
-            <div class="py-4" :class="event.team_type === 'A'? `text-left pl-4`: `text-right pr-4`">
-              <v-icon class="mb-2 mr-2">{{searchProperTimelineIcon(event.event_type)}}</v-icon>
-              <span class="event__type-text">{{ event.event_type}}</span>
-              <span
-                v-if="event.lastPlayer"
-                class="event__subType-text"
-              >- {{ searchProperEventType(event.event_type) }}</span>
-              <div class="white--text">
-                <span>{{ event.firstPlayer}}</span>
-                <span v-if="event.lastPlayer">- {{ event.lastPlayer}}</span>
-              </div>
-            </div>
-          </v-timeline-item>
-        </v-timeline>
+        <v-row v-resize="onResize">
+          <v-col cols="12" sm>
+            <v-timeline :dense="windowSize.x < 400 ? true : false">
+              <v-timeline-item
+                v-for="(event, i) in gameEventList"
+                :key="i"
+                color="#187c5a"
+                fill-dot
+                small
+                :right="event.team_type === 'A' ? true : false"
+                :left="event.team_type === 'H' ? true : false"
+              >
+                <template v-slot:opposite>
+                  <span class="event__time-text" v-text="event.event_time"></span>
+                </template>
+                <div
+                  class="py-4 text-left"
+                  :class="windowSize.x < 400 ? `` : event.team_type === 'A' ? `pl-4`: `text-right pr-4`"
+                >
+                  <div v-if="windowSize.x < 400" class="event__time-text" v-text="event.event_time"></div>
+                  <v-icon
+                    :small="windowSize.x < 400 ? true : false"
+                    class="mb-2 mr-2"
+                  >{{searchProperTimelineIcon(event.event_type)}}</v-icon>
+                  <span class="event__type-text">{{ event.event_type}}</span>
+                  <span
+                    v-if="event.lastPlayer"
+                    class="event__subType-text"
+                  >- {{ searchProperEventType(event.event_type) }}</span>
+                  <div class="white--text">
+                    <span>{{ event.firstPlayer}}</span>
+                    <span v-if="event.lastPlayer">- {{ event.lastPlayer}}</span>
+                  </div>
+                </div>
+              </v-timeline-item>
+            </v-timeline>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-col>
@@ -70,10 +81,19 @@ export default {
         color: "orange",
         year: "2000"
       }
-    ]
+    ],
+    windowSize: {
+      x: 0,
+      y: 0
+    }
   }),
-  computed: {},
+  mounted() {
+    this.onResize();
+  },
   methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight };
+    },
     searchProperTimelineIcon: function(iconType) {
       return matchValue.gameReportEventTimeLineIcon[iconType];
     },
