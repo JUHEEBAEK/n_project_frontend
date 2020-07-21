43e97bb6ed628f9f5f3458acc307c8f0aaeb4dd3
Juhee Baek <juhee.baek@fitogether.com>
2020-07-21 17:57:03 +0900

    dialog에 필요한 것들 추가 후 선택 가능한 범위 제한

--- src/components/member/TableList.vue
+++ src/components/member/TableList.vue
@@ -1,8 +1,70 @@
 <template>
   <v-data-table :headers="headers" :items="searchResult" sort-by="joinDate" class="elevation-1">
+    <template v-slot:top>
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ formTitle }}</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-text-field v-model="editedItem.name" label="name"></v-text-field>
+                </v-col>
+                <!-- <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.join_date" label="joinDate"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="editedItem.uniform_number" label="uniformNumber"></v-text-field>
+                </v-col>-->
+                <v-col cols="12" sm="6">
+                  <v-text-field
+                    type="number"
+                    min="1"
+                    max="3"
+                    v-model="editedItem.grade"
+                    label="grade"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-menu
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        :v-model="editedItem.withdraw_date"
+                        label="withdrawDate"
+                        hide-details
+                        prepend-icon="fas fa-calendar-alt"
+                        :value="date"
+                        v-on="on"
+                      />
+                    </template>
+                    <v-date-picker v-model="date" @input="menu = false" />
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
     <template v-slot:item.actions="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)">fas fa-pencil-alt</v-icon>
-      <v-icon small @click="deleteItem(item)">fas fa-trash-alt</v-icon>
+      <v-icon small @click="deleteMember(item)">fas fa-trash-alt</v-icon>
     </template>
     <template v-slot:no-data>
       <v-btn color="primary" @click="initialize">Reset</v-btn>
@@ -31,6 +93,8 @@ export default {
     this.select_all_member();
   },
   data: () => ({
+    menu: false,
+    date: "",
     dialog: false,
     headers: [
       {
