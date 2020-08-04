5d98b5793d50fbc7860da0c62e0ab5055116bcd0
dmsghs92 <dmsghs92@gmail.com>
2020-08-05 04:07:55 +0900

    Move Map from Home to Stadium

--- src/views/nav/stadium/Stadium.vue
+++ src/views/nav/stadium/Stadium.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <stadium-toolbar />
-    <stadium-cardList />
+    <v-row>
+      <v-col cols="6">
+        <div class="scrollable">
+          <stadium-cardList />
+        </div>
+      </v-col>
+      <v-col cols="6">
+        <stadium-map />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -9,9 +18,20 @@
 export default {
   name: "stadiumList",
   data: () => ({
-    title: "STADIUM"
-  })
+    title: "STADIUM",
+    scrollInvoked: 0
+  }),
+  methods: {
+    onScroll() {
+      this.scrollInvoked++;
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.scrollable {
+  overflow: auto;
+  max-height: 100vh;
+}
+</style>
