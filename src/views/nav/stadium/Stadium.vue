d264b5d374a708459c10c414aa2723e08b820382
Cheol_Kim <cheol.kim@fitogether.com>
2020-08-08 15:13:04 +0900

    Remove Code for Test

--- src/views/nav/stadium/Stadium.vue
+++ src/views/nav/stadium/Stadium.vue
@@ -3,7 +3,7 @@
     <stadium-toolbar />
     <v-row>
       <v-col cols="12" md="9" lg="9" xl="9">
-        <stadium-map :selectedStadiumIndex="selectedStadiumIndex" :showStadium="true" :makeMarkerWithClick="true"  v-model="markerPosition"/>
+        <stadium-map :selectedStadiumIndex="selectedStadiumIndex" :showStadium="true" :makeMarkerWithClick="false"/>
       </v-col>
       <v-col cols="12" md="3" lg="3" xl="3">
         <stadium-cardList @selectStadium="handleSelect"  />
@@ -18,10 +18,6 @@ export default {
   data: () => ({
     title: "STADIUM",
     selectedStadiumIndex: null,
-    markerPosition:{
-      latitude: 37.54393,
-      longitude: 127.071252
-    }
   }),
   methods: {
     handleSelect(index) {
