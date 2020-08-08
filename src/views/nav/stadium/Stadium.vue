87d4f24e222cfd539f1933ce76927eae02fd0d4f
Cheol_Kim <cheol.kim@fitogether.com>
2020-08-08 14:44:15 +0900

    Add v-model to Map - Can move marker with click

--- src/views/nav/stadium/Stadium.vue
+++ src/views/nav/stadium/Stadium.vue
@@ -3,10 +3,10 @@
     <stadium-toolbar />
     <v-row>
       <v-col cols="12" md="9" lg="9" xl="9">
-        <stadium-map :selectedStadiumIndex="selectedStadiumIndex" />
+        <stadium-map :selectedStadiumIndex="selectedStadiumIndex" :showStadium="true" :makeMarkerWithClick="true"  v-model="markerPosition"/>
       </v-col>
       <v-col cols="12" md="3" lg="3" xl="3">
-        <stadium-cardList @selectStadium="handleSelect" />
+        <stadium-cardList @selectStadium="handleSelect"  />
       </v-col>
     </v-row>
   </div>
@@ -17,7 +17,11 @@ export default {
   name: "stadiumList",
   data: () => ({
     title: "STADIUM",
-    selectedStadiumIndex: null
+    selectedStadiumIndex: null,
+    markerPosition:{
+      latitude: 37.54393,
+      longitude: 127.071252
+    }
   }),
   methods: {
     handleSelect(index) {
