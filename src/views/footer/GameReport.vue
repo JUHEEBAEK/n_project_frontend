0ffb02f997dda773f6c805c14aec29742bd846d0
Juhee <juhee.baek@fitogether.com>
2020-11-10 20:43:58 +0900

    리그전 버튼 안보이도록

--- src/views/footer/GameReport.vue
+++ src/views/footer/GameReport.vue
@@ -2,9 +2,6 @@
   <div class="gameReport__container ma-3">
     <v-app-bar prominent flat color="white">
       <v-row>
-        <v-col cols="12" xs="12" sm="12" md="2" lg="2" xl="2">
-          <v-btn outlined @click="movePage">리그전</v-btn>
-        </v-col>
         <v-col cols="6" xs="6" sm="6" md="3" lg="2" xl="2">
           <v-select v-model="nowYear" :items="years" label="년도 선택" solo rounded />
         </v-col>
