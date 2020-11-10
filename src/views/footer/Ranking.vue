1d91cccff4ec8ff9ca4ba653b98ac3fe603c1df3
Juhee <juhee.baek@fitogether.com>
2020-11-10 20:43:12 +0900

    랭킹 팀페이지 순위 변경

--- src/views/footer/Ranking.vue
+++ src/views/footer/Ranking.vue
@@ -27,16 +27,16 @@
       </div>
       <div class="content__general" v-if="tabs === 'PERSONAL'">
         <ranking-filter @filterChanged="filterRanking"/>
-        <v-card color="basil">
+        <v-card>
           <v-card-title class="text-center justify-center py-6">
-            <h1 class="font-weight-bold display-3 basil--text">N-Ranking</h1>
+            <h1 class="font-weight-bold display-3">N-Ranking</h1>
           </v-card-title>
           <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
             <v-tab v-for="(item, index) in tabItems" :key="index">{{ item }}</v-tab>
           </v-tabs>
           <v-tabs-items v-model="tab">
             <v-tab-item v-for="(item, index) in tabItems" :key="index">
-              <v-card color="basil" flat>
+              <v-card flat>
                 <ranking-table :tableData="rankingData[item]"></ranking-table>
               </v-card>
             </v-tab-item>
