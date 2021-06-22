c234982a4c69f08af975f23fc511f9f5cd185d01
Juhee <juhee.baek@fitogether.com>
2021-06-22 19:50:09 +0900

    [chore] meta 태그 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 /* 1. 목적 : 로그인을 하지 않아도 접근 가능한 정보를 요청하는 경우에 사용하기 위한 목적
    2. 설명 : header 에 token 을 담지 않는다.
    3. 참고 : "get" 의 경우에는 data 를 " " 처럼 공백으로 보내고. url 에 해당 params 를 담아서 요청하면 된다. */
@@ -9,7 +8,6 @@ export async function generalReq(method, url, data) {
     process.env.NODE_ENV === "development"
   ? "http://localhost:3000"
   : "http://52.78.180.164:3000";
-  
   axios.defaults.baseURL = DOMAIN;
   try {
     const res = await axios({
@@ -32,15 +30,15 @@ export async function authReq(method, url, token, data) {
   : "http://52.78.180.164:3000";
   
   axios.defaults.baseURL = DOMAIN;
+  axios.defaults.headers.common = { "x-access-token": token };
   try {
-    console.log("~~~~~~~~~ authReq ~~~~~~~~");
-    console.log("token", token);
-    axios.defaults.headers.common = { "x-access-token": token };
+    // axios.defaults.headers.common = { "x-access-token": token };
     const res = await axios({
       method,
       url,
       data : data
     });
+    console.log("res",res);
     return res;
   } catch (err) {
       console.log(err);
