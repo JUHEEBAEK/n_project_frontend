f244443fa7a90cbc1e24c01672c2fafb8d95b0bd
김철 <dmsghs92@gmail.com>
2021-08-01 10:19:36 +0900

    데이터 불러왔을 때 보여주기

--- src/components/squad/InputPosition.vue
+++ src/components/squad/InputPosition.vue
@@ -1,35 +1,52 @@
 <template>
   <div>
-    <v-row justify="center">
-      <v-chip
-        v-for="(member, index) in fieldMembers"
-        class="ma-2"
-        color="indigo"
-        text-color="white"
-        :key="index"
-      >
-        {{ member.name }}
-      </v-chip>
+    <v-row justify="center" align="center">
+      <v-col cols="2">
+        <span> 필드: </span>
+      </v-col>
+      <v-col cols="10">
+        <v-chip
+          v-for="(member, index) in fieldMembers"
+          class="ma-2"
+          color="indigo"
+          text-color="white"
+          :key="index"
+        >
+          {{ member.name }}
+        </v-chip>
+      </v-col>
     </v-row>
-    <v-row justify="center">
-      <squad-input-chip-combobox
-        v-model="goalKeeper"
-        :items="members"
-        label="골키퍼 선택"
-      />
+    <v-row justify="center" align="center">
+      <v-col cols="2">
+        <span> 골기퍼: </span>
+      </v-col>
+      <v-col>
+        <squad-input-chip-combobox
+          v-model="goalKeeper"
+          :items="members"
+          label="골키퍼 선택"
+        />
+      </v-col>
     </v-row>
-    <v-row justify="center">
-      <squad-input-chip-combobox
-        v-model="benchMembers"
-        multiple
-        :items="members"
-        label="벤치 선택"
-      />
+    <v-row justify="center" align="center">
+      <v-col cols="2">
+        <span> 벤치: </span>
+      </v-col>
+      <v-col>
+        <squad-input-chip-combobox
+          v-model="benchMembers"
+          multiple
+          :items="members"
+          label="벤치 선택"
+        />
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import positionValue from "@/assets/value/position.json";
+
 export default {
   props: {
     members: {
@@ -39,23 +56,45 @@ export default {
   },
   data: () => ({
     goalKeeper: null,
-    benchMembers: []
+    benchMembers: [],
+    jockerMembers: []
   }),
   computed: {
     fieldMembers() {
-      let filteredMember = [];
+      let fieldMembers = [];
       this.members.forEach(member => {
         if (
           !this.benchMembers.includes(member) &&
-          !(this.goalKeeper == member)
+          !(this.goalKeeper == member) &&
+          !this.jockerMembers.includes(member)
         ) {
-          filteredMember.push(member);
+          fieldMembers.push(member);
         }
       });
-      return filteredMember;
+
+      return fieldMembers;
     }
   },
   watch: {
+    members: {
+      immediate: true,
+      handler: function(members) {
+        members.forEach(member => {
+          if (member.position === "GK") {
+            this.goalKeeper = member;
+          }
+          if (positionValue.benchList.includes(member.position)) {
+            this.benchMembers.push(member);
+          }
+          if (member.position === "JK") {
+            this.jockerMembers.push(member);
+          }
+        });
+      }
+    },
+    fieldMembers() {
+      this.emitData();
+    },
     goalKeeper(member) {
       if (member) {
         let colapseIndex = this.benchMembers.indexOf(member);
@@ -72,7 +111,20 @@ export default {
       });
     }
   },
-  methods: {}
+  methods: {
+    emitData() {
+      let members = [];
+      this.fieldMembers.forEach(member => {
+        members.push({ ...member });
+      });
+      if (this.goalKeeper) members.push({ ...this.goalKeeper, position: "GK" });
+      this.benchMembers.forEach((member, index) => {
+        members.push({ ...member, position: positionValue.benchList[index] });
+      });
+
+      this.$emit("change", members);
+    }
+  }
 };
 </script>
 
