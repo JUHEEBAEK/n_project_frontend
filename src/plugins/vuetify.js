3a3c4bc3c5f7d8a04da6dd68acd5aac19c036daf
Juhee <juhee.baek@fitogether.com>
2021-08-23 23:21:47 +0900

    [fix] font awesome => mdi icon 사용
    issue#76

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,16 +1,10 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import theme from "./theme";
-import "@fortawesome/fontawesome-free/css/all.css";
-
-Vue.use(Vuetify);
 
 Vue.use(Vuetify);
 
 const opts = {
-  icons: {
-    iconfont: "fa"
-  },
   theme
 };
 
